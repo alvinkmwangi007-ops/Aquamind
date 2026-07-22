8c206d1aae44640c054761a611c6c757c23ed68c
Alvin Kamau <alvink.mwangi@gmail.com>
2026-07-22 18:06:30 +0300

    added the codebase to the Header.jsx and also the Header.css ,which also contains the logo of the aquamind app

---
+++ aquamind/src/components/Header.jsx
@@ -0,0 +1,12 @@
+// src/components/Header.jsx
+import './Header.css';
+import logo from "../assets/waterdrop.png";
+
+export default function Header() {
+  return (
+    <header className="header">
+      <img src={logo} alt="AquaMind logo" className="logo" />
+      <h1>AquaMind</h1>
+    </header>
+  );
+}
