30afbe4813008bb395efe45aaf81f113c9b62c12
Alvin Kamau <alvink.mwangi@gmail.com>
2026-07-22 18:12:23 +0300

    the Daily LOgging in form has been created with its counterpart css file .It is a file to enable proggress tracking

---
+++ aquamind/src/components/DailyLogForm.jsx
@@ -0,0 +1,27 @@
+// src/components/DailyLogForm.jsx
+import { useState } from "react";
+import './DailyLogForm.css';
+
+export default function DailyLogForm({ onAdd }) {
+  const [amount, setAmount] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (amount) {
+      onAdd(Number(amount));
+      setAmount("");
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className="daily-form card">
+      <input
+        type="number"
+        value={amount}
+        onChange={(e) => setAmount(e.target.value)}
+        placeholder="Enter water (ml)"
+      />
+      <button type="submit">Add</button>
+    </form>
+  );
+}
