4ef7b6bb35cc8d912f6a45f37c9387505fa6bcfd
Alvin Kamau <alvink.mwangi@gmail.com>
2026-07-24 12:54:24 +0300

    addition of the react router inorder to clearly define the separation of pages

--- aquamind/src/components/Header.jsx
+++ aquamind/src/components/Header.jsx
@@ -1,13 +1,17 @@
-// src/components/Header.jsx
-
-import '../styles/Header.css';
-import logo from '../assets/waterdrop.png';
+import { Link } from "react-router-dom";
+import "../styles/Header.css";
+import logo from "../assets/waterdrop.png";
 
 export default function Header() {
   return (
     <header className="header">
       <img src={logo} alt="AquaMind logo" className="logo" />
       <h1>AquaMind</h1>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/history">History</Link>
+        <Link to="/settings">Settings</Link>
+      </nav>
     </header>
   );
 }
